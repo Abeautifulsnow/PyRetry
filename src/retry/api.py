519bf4b774a7c392f4a06b40e71b84839722ad6d
Abeautifulsnow <1491444340@qq.com>
2022-09-14 23:22:11 +0800

    update

--- src/retry/api.py
+++ src/retry/api.py
@@ -7,18 +7,18 @@ from typing import Callable, Union, Tuple, Optional, NewType, Any
 from .compat import decorator
 
 logging_logger = logging.getLogger(__name__)
-T_Exception = NewType('T_Exception', Exception)
+T_Exception = NewType("T_Exception", Exception)
 
 
 def __retry_internal(
-        f: Callable,
-        exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
-        tries: int = -1,
-        delay: int = 0,
-        max_delay: Optional[int] = None,
-        backoff: int = 1,
-        jitter: Union[int, Tuple[int, ...]] = 0,
-        logger: Optional[logging.Logger] = logging_logger
+    f: Callable,
+    exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
+    tries: int = -1,
+    delay: int = 0,
+    max_delay: Optional[int] = None,
+    backoff: int = 1,
+    jitter: Union[int, Tuple[int, ...]] = 0,
+    logger: Optional[logging.Logger] = logging_logger,
 ) -> Optional[Callable]:
     """
     Executes a function and retries it if it failed.
@@ -46,7 +46,7 @@ def __retry_internal(
                 raise
 
             if logger:
-                logger.warning(f'{e}, retrying in {_delay} seconds...')
+                logger.warning(f"{e}, retrying in {_delay} seconds...")
 
             time.sleep(_delay)
             _delay *= backoff
@@ -61,13 +61,13 @@ def __retry_internal(
 
 
 def retry(
-        exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
-        tries: int = -1,
-        delay: int = 0,
-        max_delay: Optional[int] = None,
-        backoff: int = 1,
-        jitter: Union[int, Tuple[int, ...]] = 0,
-        logger: Optional[logging.Logger] = logging_logger
+    exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
+    tries: int = -1,
+    delay: int = 0,
+    max_delay: Optional[int] = None,
+    backoff: int = 1,
+    jitter: Union[int, Tuple[int, ...]] = 0,
+    logger: Optional[logging.Logger] = logging_logger,
 ) -> Callable:
     """Returns a retry decorator.
 
@@ -82,10 +82,11 @@ def retry(
                    default: retry.logging_logger. if None, logging is disabled.
     :returns: a retry decorator.
     """
+
     @decorator
-    def retry_decorator(f: Callable, *fargs, **fkwargs):
-        args = fargs if fargs else []
-        kwargs = fkwargs if fkwargs else {}
+    def retry_decorator(f: Callable, *f_args, **f_kwargs):
+        args = f_args if f_args else []
+        kwargs = f_kwargs if f_kwargs else {}
         return __retry_internal(
             partial(f, *args, **kwargs),
             exceptions,
@@ -94,30 +95,30 @@ def retry(
             max_delay,
             backoff,
             jitter,
-            logger
+            logger,
         )
 
     return retry_decorator
 
 
 def retry_call(
-        f: Callable,
-        fargs: Optional[Any] = None,
-        fkwargs: Optional[Any] = None,
-        exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
-        tries: int = -1,
-        delay: int = 0,
-        max_delay: Optional[int] = None,
-        backoff: int = 1,
-        jitter: Union[int, Tuple[int, ...]] = 0,
-        logger: Optional[logging.Logger] = logging_logger
+    f: Callable,
+    f_args: Optional[Any] = None,
+    f_kwargs: Optional[Any] = None,
+    exceptions: Union[T_Exception, Tuple[T_Exception, ...]] = Exception,
+    tries: int = -1,
+    delay: int = 0,
+    max_delay: Optional[int] = None,
+    backoff: int = 1,
+    jitter: Union[int, Tuple[int, ...]] = 0,
+    logger: Optional[logging.Logger] = logging_logger,
 ) -> Callable:
     """
     Calls a function and re-executes it if it failed.
 
     :param f: the function to execute.
-    :param fargs: the positional arguments of the function to execute.
-    :param fkwargs: the named arguments of the function to execute.
+    :param f_args: the positional arguments of the function to execute.
+    :param f_kwargs: the named arguments of the function to execute.
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
     :param tries: the maximum number of attempts. default: -1 (infinite).
     :param delay: initial delay between attempts. default: 0.
@@ -129,8 +130,8 @@ def retry_call(
                    default: retry.logging_logger. if None, logging is disabled.
     :returns: the result of the f function.
     """
-    args = fargs if fargs else []
-    kwargs = fkwargs if fkwargs else {}
+    args = f_args if f_args else []
+    kwargs = f_kwargs if f_kwargs else {}
     return __retry_internal(
         partial(f, *args, **kwargs),
         exceptions,
@@ -139,5 +140,5 @@ def retry_call(
         max_delay,
         backoff,
         jitter,
-        logger
+        logger,
     )
--- src/retry/compat.py
+++ src/retry/compat.py
@@ -4,16 +4,19 @@ from typing import Callable
 
 
 def decorator(caller: Callable):
-    """ Turns caller into a decorator.
+    """Turns caller into a decorator.
     Unlike decorator module, function signature is not preserved.
 
     :param caller: caller(f, *args, **kwargs)
     """
+
     def decor(f: Callable):
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
             return caller(f, *args, **kwargs)
+
         return wrapper
+
     return decor
 
 
